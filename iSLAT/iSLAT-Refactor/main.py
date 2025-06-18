0ac6b591924850f478f1ba8bdbcd26c8d50b3396
asiemsen <siemsenaaron@gmail.com>
2025-06-17 21:13:43 -0500

    Added constants file

--- iSLAT/iSLAT-Refactor/main.py
+++ iSLAT/iSLAT-Refactor/main.py
@@ -1,10 +1,21 @@
 #new entry point for iSLAT
 import tkinter as tk
 
-from core.core import selectfileinit
-
+from mixed.mixed import selectfileinit
+from core.core import read_from_user_csv
 
 root = tk.Tk ()
 root.withdraw ()
 root.call ('wm', 'attributes', '.', '-topmost', True)
+
+selectfileinit()
+
+print (' ')
+print ('Loading molecule files: ...')
+
+molecules_data = read_from_user_csv()
+
+#MAKE CONSTANT CLASS TO IMPORT
+
+
 
